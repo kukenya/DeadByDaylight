930b0d790805ecb009bb3b397ba14611a1b04e70
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-08-16 20:32:36 +0900

    로비
    로비

---
+++ Assets/00.Personal/02.KJJ/01.Scripts/Button.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public GameObject lobby;
+    public GameObject killerLobby;
+    public GameObject playerLobby;
+
+    public GameObject killer;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) OnclickBack();
+    }
+
+    public void OnclickKiller()
+    {
+        player.SetActive(false);
+        killer.SetActive(true);
+        lobby.SetActive(false);
+        killerLobby.SetActive(true);
+    }
+
+    public void OnclickPlayer()
+    {
+        killer.SetActive(false);
+        player.SetActive(true);
+        lobby.SetActive(false);
+        playerLobby.SetActive(true);
+    }
+
+    public void OnclickBack()
+    {
+        killer.SetActive(false);
+        player.SetActive(false);
+        killerLobby.SetActive(false);
+        playerLobby.SetActive(false);
+        lobby.SetActive(true);
+    }
+}
