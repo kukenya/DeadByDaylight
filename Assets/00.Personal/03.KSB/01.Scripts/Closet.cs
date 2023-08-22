3edbca201a52cd3e87fb3a38a291cf8c63c9aa19
JoanneKael <JoanneKael@users.noreply.github.com>
2023-08-22 17:36:53 +0900

    o

---
+++ Assets/00.Personal/03.KSB/01.Scripts/Closet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 만약 게임오브젝트가 두 개 이상이라면 그 게임오브젝트를 배열에 담는다.
+// 나와 거리를 재서 가까운 순서로 재정렬한다.
+// 가장 가까운 게임오브젝트 (인덱스 0번) 를 closetSpot 으로 한다.
+public class Closet : MonoBehaviour
+{
+    public Transform trCloset;     // closet Transform
+
+}
