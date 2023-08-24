50b339466a71bc16effa1233b739713bc8ae3710
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-08-24 13:46:58 +0900

    .

--- Assets/00.Personal/02.KJJ/01.Scripts/ButtonClick.cs
+++ Assets/00.Personal/02.KJJ/01.Scripts/ButtonClick.cs
@@ -1,20 +1,21 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonClick : MonoBehaviour
+public class ButtonClick : MonoBehaviourPun
 {
     public GameObject lobby;
     public GameObject killerLobby;
     public GameObject playerLobby;
-    public GameObject ready;
-    public GameObject cancel;
+    public GameObject playerReady;
+    public GameObject playerCancel;
+    public GameObject readycheck;
 
     public GameObject killer;
     public GameObject player;
-
 
     // Start is called before the first frame update
     void Start()
@@ -60,15 +61,23 @@ public class ButtonClick : MonoBehaviour
         lobby.SetActive(true);
     }
 
-    public void Ready()
+    public void PlayerReady()
     {
-        cancel.SetActive(true);
-        ready.SetActive(false);
-    }
+        playerCancel.SetActive(true);
+        playerReady.SetActive(false);
+        readycheck.SetActive(true);
+        LobbyManager.instance.SetReady(true);
 
-    public void Cancel()
-    {
-        ready.SetActive(true);
-        cancel.SetActive(false);
+        //LobbyManager.instance.myPhotonView.RPC("RpcSetReady", Photon.Pun.RpcTarget.All, true);
     }
+    
+
+    public void PlayerCancel()
+    {
+        playerReady.SetActive(true);
+        playerCancel.SetActive(false);
+        readycheck.SetActive(false);
+        LobbyManager.instance.SetReady(false);
+    }
+    
 }
